448a8206391350f915519c54565073ace035a90c
Alex Armstrong <alexarmstrong999@gmail.com>
2020-12-05 08:45:44 -0600

    Modified purchaseHistory stuff

--- routes/checkout.js
+++ routes/checkout.js
@@ -12,6 +12,8 @@ router.post('/', function(req,res,next){
     collection.findOne({username: req.session.user}, function(err, user){
       //send message if quantity not enough
       let approvedItems =[]
+      //check if each item has enough in stock
+
       user.cart.forEach(function(item){
         games.findOne({_id: item.gameID}, function(err, game){
           if(err) throw err;
@@ -24,12 +26,21 @@ router.post('/', function(req,res,next){
           }
         });
       });
+
+
+      // create purchase history entry + reduce item stock quantity by purchase quantity
+      let currentHistory = []
+      let totalPrice = 0;
       approvedItems.forEach(function(item){
         games.findOne({_id: item.gameID}, function(err, game){
           games.findOneAndUpdate({_id: item.gameID},{$set: {quantity: game.quantity-item.quantity}});
+          currentHistory.push(item);
+          totalPrice += item.quantity * item.price;
         }
         );
       });
+      user.purchaseHistory.push({items: currentHistory, total: totalPrice});
+      collection.findOneAndUpdate({username: req.session.user}, {$set: {purchaseHistory: user.purchaseHistory}});
       res.render('thanks',{username: req.session.user});
     });
   });
