f7a0602785e6556d5bae43a28886fed34feb5e81
razvanpreotu <rnp170130@utdallas.edu>
2020-12-06 22:37:48 -0500

    final version

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,12 +1,8 @@
 $(function(){
-    //console.log(category);
-    //console.log(search);
-
     if(typeof(category) != undefined)
         $("#category").val("default");
     else
         $("#category").val(category);
 
-    console.log(search);
     $("#title").val(search);
-})
+});
